160dfb5c5ba0b3e5051e84a95d38e544bbcf7b7e
Jakub Szaredko <jakub.szaredko@gmail.com>
2020-06-26 00:21:23 +0200

    Created a field script

---
+++ Assets/Scripts/Field.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Field : MonoBehaviour
+{
+    public enum Ownership { Player, Neutral, Enemy }
+
+    public Ownership ownership;
+
+    SpriteRenderer border;
+    SpriteRenderer fill;
+    
+    void Start()
+    {
+        border = transform.Find("Border").GetComponent<SpriteRenderer>();
+        fill = transform.Find("Fill").GetComponent<SpriteRenderer>();
+
+        Color c = Color.white;
+        switch (ownership)
+        {
+            case Ownership.Player:
+                c = new Color(0.254f, 0.823f, 0.976f);
+                break;
+            case Ownership.Neutral:
+                c = new Color(0.670f, 0.647f, 0.749f);
+                break;
+            case Ownership.Enemy:
+                c = new Color(0.968f, 0.262f, 0.262f);
+                break;
+        }
+        fill.color = c;
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
