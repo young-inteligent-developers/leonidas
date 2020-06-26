5ce28144429acd8e82b0531b22a59e1b764f5198
Jakub Szaredko <jakub.szaredko@gmail.com>
2020-06-26 22:40:21 +0200

    Updated InputManager.cs

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -4,12 +4,11 @@ public class InputManager : MonoBehaviour
 {
     void Update()
     {
-        if (Input.touchCount == 0) return;
-
-        Touch t = Input.GetTouch(0);
-        if (t.phase == TouchPhase.Ended)
+#if UNITY_EDITOR
+        if (Input.GetMouseButtonUp(0))
         {
-            RaycastHit2D hit = Physics2D.Raycast(t.position, Vector2.up, 0.01f);
+            Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.up, 0.01f);
             if (!hit) return;
 
             if (hit.transform.tag == "Field")
@@ -17,5 +16,22 @@ public class InputManager : MonoBehaviour
                 hit.transform.GetComponent<Field>().HandleClick();
             }
         }
+#elif UNITY_ANDROID || UNITY_IOS
+        if (Input.touchCount == 0) return;
+
+        Touch t = Input.GetTouch(0);
+        if (t.phase == TouchPhase.Ended)
+        {
+            Vector2 pos = Camera.main.ScreenToWorldPoint(t.position);
+            RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.up, 0.01f);
+            if (!hit) return;
+
+            if (hit.transform.tag == "Field")
+            {
+                hit.transform.GetComponent<Field>().HandleClick();
+            }
+        }
+#endif
     }
+
 }
