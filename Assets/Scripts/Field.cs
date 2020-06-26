e96179c34315e15500ac4c5803ad5652204f2552
Jakub Szaredko <jakub.szaredko@gmail.com>
2020-06-26 15:43:22 +0200

    Updated a field script

--- Assets/Scripts/Field.cs
+++ Assets/Scripts/Field.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
-
 
 public class Field : MonoBehaviour
 {
@@ -21,24 +21,30 @@ public class Field : MonoBehaviour
         fill = transform.Find("Fill").GetComponent<SpriteRenderer>();
         canvas = GameObject.Find("Canvas").transform;
 
-        Color c = Color.white;
+        Color[] c = { Color.white, Color.white };
         switch (ownership)
         {
             case Ownership.Player:
-                c = new Color(0.254f, 0.823f, 0.976f);
+                c[0] = new Color(0.254f, 0.823f, 0.976f);
+                c[1] = new Color(0.501f, 0.905f, 0.976f);
                 break;
             case Ownership.Neutral:
-                c = new Color(0.670f, 0.647f, 0.749f);
+                c[0] = new Color(0.670f, 0.647f, 0.749f);
+                c[1] = new Color(0.768f, 0.756f, 0.827f);
                 break;
             case Ownership.Enemy:
-                c = new Color(0.968f, 0.262f, 0.262f);
+                c[0] = new Color(0.968f, 0.262f, 0.262f);
+                c[1] = new Color(0.976f, 0.450f, 0.450f);
                 break;
         }
-        fill.color = c;
+        fill.color = c[0];
 
         Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
-        GameObject text = Instantiate(strengthText, canvas);
-        text.GetComponent<RectTransform>().position = pos;
+        GameObject strengthUi = Instantiate(strengthText, canvas);
+        strengthUi.GetComponent<RectTransform>().position = pos;
+        Image background = strengthUi.transform.GetChild(0).GetComponent<Image>();
+        background.color = c[1];
+        Transform text = strengthUi.transform.GetChild(1);
         text.GetComponent<TextMeshProUGUI>().text = strength.ToString();
     }
 
